075ae9809c462f9960d0286b95ee07a7f678327a
johnarvi <arvind.john@mail.utoronto.ca>
2019-10-21 16:01:07 +0000

    refactored and created separate cases for tests and import files

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,3 @@
-// test 1
-// const sum = function(a, b) {
-//   return a * b;
-// }
-
-// console.assert(sum(1, 2) === 3);
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
@@ -12,7 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+
+module.exports = assertEqual;
