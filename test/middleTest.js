8ea58595dc0b34812afc2a8476ba2fd4c557ce21
johnarvi <arvind.john@mail.utoronto.ca>
2019-10-21 19:24:18 +0000

    added mocha and chai assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const middle = require("../middle");
+const assert = require('chai').assert;
+const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("return the middle values in an index", () => {
+  it('returns [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('returns [2] for odd array length [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('returns [3] for odd array length [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('returns [2, 3] for even array length [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('should not return undefined for [1]', () => {
+    assert.notDeepEqual(middle([1]), undefined);
+  });
+  it('should not return undefined for [1, 2]', () => {
+    assert.notDeepEqual(middle([1, 2]), undefined);
+  });
+  it('should not return undefined for []', () => {
+    assert.notDeepEqual(middle([]), undefined);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,23 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail.js');
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), 'Labs');
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const words = ["Yo Yo"];
-//const words = [];
-// console.log(tail(words));
-//assertEqual(words.length, 3);
+describe("return the tail(last) value in an index", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns [] for ["Yo Yo"]', () => {
+    assert.deepEqual(tail(["Yo Yo"]), []);
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it('should not return ["Hello"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.notDeepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Hello"]);
+  });
+  it('should not return undefined for ["Yo Yo"]', () => {
+    assert.notDeepEqual(tail(["Yo Yo"]), undefined);
+  });
+  it('should not return undefined for []', () => {
+    assert.notDeepEqual(tail([]), undefined);
+  });
+});
